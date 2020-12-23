7faced52ae80cd6ec166fa228f74c973fc71595e
F36 <70635838+F36@users.noreply.github.com>
2020-12-23 18:34:36 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -55,7 +55,7 @@ Hi {}, my name is {}!
 I am an Anime themed group management bot with a lot of Special Features.
 You can find the list of available commands with /help.
 ==========================
--> [Source](https://github.com/F36UzakiChanBot)
+-> [Source](https://github.com/F36/UzakiChanBot)
 -> Report To [AnimeChatOfficial](https://t.me/AnimeChatOfficial) if I go offline
 ==========================
 `Maintained by` @AnimeIndexOfficial
@@ -81,7 +81,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/6d8bf6545ab33a427b989.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/372d68a5e64b1513ca466.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 I am hosted on one of Heroku's Servers and doesn't require any donations as of now but \
